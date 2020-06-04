a1dd0adc2954a176a16e8288fb808d4e128cc2f0
ashutosh-7 <accesssdenied777@gmail.com>
2020-06-05 02:20:57 +0530

    June 5

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const express = require('express'); //importing express
-const app = express();  //making app as express object
 const path = require('path');
 const rootDir = require('./util/path.js'); //for getting root directory path
-
 const adminRoutes = require('./routes/admin');  //for admin routes importing
 const shopRoutes = require('./routes/shop'); //for importing shop routes which is for users panel
-
 const bodyParser =require('body-parser');  //importing body-parser which takes input stream from html forms
 
-app.use(bodyParser.urlencoded({extended : false})); //parse form bodies
 
-app.use(express.static(path.join(rootDir,'public')));
+const app = express();  //making app as express object
+
+app.use(bodyParser.urlencoded({extended : false})); //parse form bodies
+app.use(express.static(path.join(rootDir,'public'))); //including public folder accesseible
 
 app.use('/admin',adminRoutes);  // '/admin' is a filter which we use in url
 app.use(shopRoutes);
--- routes/admin.js
+++ routes/admin.js
@@ -5,6 +5,7 @@ const path = require ('path');
 const rootDir = require('../util/path.js');
 const router= express.Router();
 
+
 router.get('/add-product',(req,res,next) => {
     
     res.sendFile(path.join(rootDir,'views','add-product.html'));
