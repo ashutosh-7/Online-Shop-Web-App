849ab0163f4b4fbff895dadb085ac4bf81fdbf79
ashutosh-7 <accesssdenied777@gmail.com>
2020-06-16 17:22:11 +0530

    Validation Completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,10 @@
+const crypto=require('crypto');
 const  connection  = require('mongoose');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const sendgridTransport = require('nodemailer-sendgrid-transport');
 const User = require('../models/user');
+const {validationResult}= require('express-validator/check');
 
 const transporter = nodemailer.createTransport(
     sendgridTransport({
@@ -51,15 +53,17 @@ exports.postSignup =(req, res , next) => {
         const email=req.body.email;
         const password=req.body.password;
         const confirmPassword=req.body.confirmPassword;
+        const errors = validationResult(req);
 
-        User.findOne({email:email})
-        .then(userDoc => {
-            if(userDoc){
-                req.flash('error','Email ALready Registered' );
-                return res.redirect('/signup'); //already exists user
-            }
-           
-            return bcrypt
+        if(!errors.isEmpty())
+        {
+             return res.status(422).render('auth/signup',{
+                pageTitle:"Signup",
+                errorMessage:errors.array()[0].msg
+                });
+        }
+
+             bcrypt
             .hash(password, 12)
             .then(hashedPassword => {
               const user = new User({
@@ -71,18 +75,18 @@ exports.postSignup =(req, res , next) => {
             })
             .then(result => {
               res.redirect('/login');
-              console.log("Sending Mails");
-              return transporter.sendMail({
+            //   console.log("Sending Mails");
+            //   return transporter.sendMail({
                   
-                to: email,
-                from: 'helloashutoshoo7@gmail.com',
-                subject: 'Signup succeeded!',
-                html: '<h1>You successfully signed up!</h1>'
-              });
+            //     to: email,
+            //     from: 'helloashutoshoo7@gmail.com',
+            //     subject: 'Signup succeeded!',
+            //     html: '<h1>You successfully signed up!</h1>'
+            //   });
             })
             .catch(err => {
               console.log(err);
-            });
+        
         })
         .catch(err => {
           console.log(err);
@@ -96,6 +100,21 @@ exports.postLogin = (req,res,next)=> {
     const email= req.body.email;
     const password= req.body.password;
     // console.log("I am here");
+    const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).render('auth/login', {
+      path: '/login',
+      pageTitle: 'Login',
+      errorMessage: errors.array()[0].msg,
+      oldInput: {
+        email: email,
+        password: password
+      },
+      validationErrors: errors.array()
+    });
+  }
+
+
     User.findOne({email:email})
     .then(user => {
         if(!user)
@@ -133,3 +152,30 @@ exports.postLogout = (req,res,next)=> {
     });
     
 };
+
+exports.getReset=(req,res,next)=> {
+    let message=req.flash('error');
+    if(message.length>0)
+    {
+        message=message[0];
+        
+    }
+    else
+    {
+        message=null;
+    }
+        res.render('auth/reset',{
+        pageTitle:"Reset Password",
+        errorMessage:message
+        });
+};
+
+exports.postReset=(req,res,next )=> {
+    crypto.randomBytes(32,(err,buffer)=> {
+        if(err)
+        {
+            return res.redirect('/reset');
+        }
+        const token = buffer.toString('hex');
+    });
+}
